9da60f753a2d6363a611b2f1ab4b03f48b8e2753
Jadon Couch <jadonc42@gmail.com>
2023-09-07 22:36:21 -0500

    Add comments

--- API/app.py
+++ API/app.py
@@ -7,6 +7,7 @@ import json
 app = Flask(__name__)
 CORS(app)
 
+# home page with routes
 @app.route("/")
 def index():
     return render_template('index.html')
@@ -15,28 +16,13 @@ def index():
 @app.route("/api/v1.0")
 def apiAll():
     return jsonify(getAll())
+
 # return data for selected year from the database
 @app.route("/api/v1.0/<year>")
 def apiYear(year):
+    # convert year to a int
     year = int(year)
     return jsonify(getYear(year))
-
-
-
-# return all data from the locally saved data
-@app.route("/api/saved")
-def savedAll():
-    with open("All-Presaved.json", "r") as file:
-        data = json.load(file)
-    return jsonify(data)
-
-# return data for selected year from the locally saved data
-@app.route("/api/saved/<year>")
-def savedYear(year):
-    with open("All-Presaved.json", "r") as file:
-        data = json.load(file)
-    filtered_data = [item for item in data if item.get("crashYear") == year]
-    return jsonify(filtered_data)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
--- API/data_collection.py
+++ API/data_collection.py
@@ -3,26 +3,42 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, text, inspect, func, MetaData, select
 
+# function to get all data from the "crashes" table
 def getAll():
+    # connect to the database
     engine = create_engine("postgresql://postgres:project3@107.172.217.213:5432/project3")
     connection = engine.connect()
+    # make request to database
     result = connection.execute(text('SELECT * FROM "Crashes"'))
     
+    # get column names from the crashes table
     metadata = MetaData()
     metadata.reflect(bind=engine, only=['Crashes'])
     column_names = metadata.tables['Crashes'].columns.keys()
 
+    # convert the data from the database into a dict
     result_dicts = [dict(zip(column_names, row)) for row in result]
+    # close the connection to the databse
+    connection.close()
+    # return the data dict
     return result_dicts
 
+# function to get all data from the "crashes" table
 def getYear(year):
+    # connect to the database
     engine = create_engine("postgresql://postgres:project3@107.172.217.213:5432/project3")
     connection = engine.connect()
+    # make request to database
     result = connection.execute(text(f'SELECT * FROM "Crashes" WHERE "crashYear" = {year}'))
     
+    # get column names from the crashes table
     metadata = MetaData()
     metadata.reflect(bind=engine, only=['Crashes'])
     column_names = metadata.tables['Crashes'].columns.keys()
 
+    # convert the data from the database into a dict
     result_dicts = [dict(zip(column_names, row)) for row in result]
+    # close the connection to the databse
+    connection.close()
+    # return the data dict
     return result_dicts
