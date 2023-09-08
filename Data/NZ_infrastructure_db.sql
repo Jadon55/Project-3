d92ab7d2d85762a05b3349a11f3a588231c7ba62
sager9 <alollywilson@gmail.com>
2023-09-08 15:42:31 -0700

    added a graph displaying the budget of public transport

--- Data/NZ_infrastructure_db.sql
+++ Data/NZ_infrastructure_db.sql
@@ -1,25 +1,14 @@
 ﻿-- Created databases to 
 
 CREATE TABLE "public_transport" (
-    "Region" string   NOT NULL,
-    "2017_18" int   NOT NULL,
-    "2018_19" int   NOT NULL,
-    "2019_20" int   NOT NULL,
-    "2020_21" int   NOT NULL,
-    "2021_22" int   NOT NULL,
+    "Region" varchar   NOT NULL,
+    "cost_2017_18" decimal   NOT NULL,
+    "cost_2018_19" decimal   NOT NULL,
+    "cost_2019_20" decimal   NOT NULL,
+    "cost_2020_21" decimal   NOT NULL,
+    "cost_2021_22" decimal   NOT NULL,
     CONSTRAINT "pk_public_transport" PRIMARY KEY (
         "Region"
      )
 );
 
-CREATE TABLE "road_maintenance" (
-    "Region" varchar   NOT NULL,
-    "2017_18" decimal   NOT NULL,
-    "2018_19" decimal   NOT NULL,
-    "2019_20" decimal   NOT NULL,
-    "2020_21" decimal   NOT NULL,
-    "2021_22" decimal   NOT NULL,
-    CONSTRAINT "pk_road_maintenance" PRIMARY KEY (
-        "Region"
-     )
-);
